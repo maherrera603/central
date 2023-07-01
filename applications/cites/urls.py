1c5eb411e45c0c47000dbddd13caf905bbed95a8
maherrera603 <maherrera603@misena.edu.co>
2023-07-01 17:05:08 -0500

    add urls and views for search cites

--- applications/cites/urls.py
+++ applications/cites/urls.py
@@ -4,9 +4,15 @@ from django.urls import path
 from .views import RegisterCiteView
 from .views import DetailCiteView
 from .views import SearchCiteView
+from .views import AllCitesView
+from .views import SearchCiteForEmployee
+from .views import DetailCiteForEmployeeView
 
 urlpatterns = [
     path("cites/", RegisterCiteView.as_view(), name="cites"),
     path("cite/<int:pk>/", DetailCiteView.as_view(), name="detail_cite"),
-    path("cite/search/<str:search>/", SearchCiteView.as_view(), name="search_cite")
+    path("cite/search/<str:search>/", SearchCiteView.as_view(), name="search_cite"),
+    path("allCites/", AllCitesView.as_view(), name="all_cites"),
+    path("cites/search/<str:search>/", SearchCiteForEmployee.as_view(), name="search_cite_for_employee"),
+    path("citeForEmployee/<int:pk>/", DetailCiteForEmployeeView.as_view(), name="detail_cite_for_employee"),
 ]
